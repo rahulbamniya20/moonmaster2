5cd381adb673dcbb114c325eaf9027411e2b3640
Rahul Bamniya <110114539+rahulbamniya20@users.noreply.github.com>
2024-08-30 22:42:11 +0530

    Updated color of main heading to bg-purple-500

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Spline from '@splinetool/react-spline';
@@ -45,7 +44,8 @@ const Hero = () => {
                 ]}
                 wrapper="span"
                 speed={50}
-                className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+                // className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+                className="text-transparent bg-clip-text  bg-purple-500"
                 repeat={Infinity}
               />
               <span className="block leading-normal">To The Moon?</span>
